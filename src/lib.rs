3436b8ef27e5683bf187f70c63c1a8bc3b417493
x0rw <mahdi.svt5@gmail.com>
2025-07-03 13:02:19 +0100

    Initial MVP

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,112 @@
-use std::{marker::PhantomData, sync::atomic::AtomicUsize};
+use std::num::NonZero;
+use std::ptr;
 
-struct IpcArc<T> {
+use nix::libc::__c_anonymous_ptp_perout_request_2;
+use nix::sys::mman::{MapFlags, ProtFlags, mmap, munmap};
+use nix::unistd::close;
+use nix::{
+    Error,
+    fcntl::OFlag,
+    libc::{self},
+    sys::{mman::shm_open, stat::Mode},
+    unistd::ftruncate,
+};
+pub struct IpcArc<T> {
     ptr: *mut T,
-    mutex: IpcArcMutex,
-    ref_count: *const AtomicUsize,
-    _phantom: PhantomData<T>,
 }
+impl<T> IpcArc<T> {
+    pub fn new() -> Self {
+        Self {
+            ptr: std::ptr::null_mut(),
+        }
+    }
+    pub fn open(&mut self, name: &str, val: T) -> Result<(), Error> {
+        let fd = shm_open(
+            name,
+            OFlag::O_CREAT | OFlag::O_RDWR,
+            Mode::from_bits_truncate(libc::S_IRUSR | libc::S_IWUSR),
+        )
+        .expect("shm_open failed");
+        let size = 64;
+
+        ftruncate(&fd, size).expect("ftruncate failed");
+        let addr = unsafe {
+            mmap(
+                None,
+                NonZero::new(20).unwrap(),
+                ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
+                MapFlags::MAP_SHARED,
+                &fd,
+                0,
+            )?
+        };
+        let nptr = addr.as_ptr() as *mut T;
+        self.ptr = nptr;
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
+
+    use std::{thread, time::Duration};
+
+    use nix::libc::fork;
+
     use super::*;
+    #[test]
+    fn test() {
+        let mut ins: IpcArc<u64> = IpcArc::new();
+        ins.open("/hello", 43).unwrap();
+
+        unsafe {
+            ptr::write(ins.ptr, 43);
+        }
+
+        let reference: &mut u64 = unsafe { &mut *ins.ptr };
+        println!("re:{}", *reference);
+
+        let s = unsafe { fork() };
+        if s == 0 {
+            thread::sleep(Duration::from_secs(4));
+
+            unsafe {
+                ptr::write(ins.ptr, 1111);
+            }
+            println!("hehe: {s}");
+            ins.open("/hello", 43).unwrap();
+
+            let reference: &mut u64 = unsafe { &mut *ins.ptr };
+            println!("re:{}", *reference);
+        } else {
+            thread::sleep(Duration::from_secs(5));
+            println!("else: {s}");
+            ins.open("/hello", 43).unwrap();
+
+            let reference: &mut u64 = unsafe { &mut *ins.ptr };
+
+            unsafe {
+                ptr::write(ins.ptr, 999);
+            }
+            println!("re:{}", *reference);
+        }
+
+        println!("re:{}", *reference);
+        // unsafe {
+        //     assert!(!ins.ptr.is_null(), "Pointer is null");
+        //     assert_eq!(
+        //         (ins.ptr as usize) % std::mem::align_of::<u64>(),
+        //         0,
+        //         "Pointer is misaligned"
+        //     );
+        //
+        //     // Write to mmaped memory safely
+        //     std::ptr::write(ins.ptr, 43u64);
+        //
+        //     // Read it back
+        //     let reference: &mut u64 = &mut *ins.ptr;
+        //     println!("re:{}", *reference);
+        // }
+    }
 }
