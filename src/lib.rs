d3629b5186b58385dbb64e20bd1a8bf01f9ad402
x0rw <mahdi.svt5@gmail.com>
2025-07-10 18:47:03 +0100

    Add Ipc Mutex, ref counter

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+use std::alloc::Layout;
+use std::mem::MaybeUninit;
 use std::num::NonZero;
-use std::ops::Add;
+use std::os::fd::{IntoRawFd, RawFd};
 use std::ptr;
 
-use nix::sys::mman::{MapFlags, ProtFlags, mmap};
+use nix::errno::Errno;
+use nix::libc::{
+    PTHREAD_PROCESS_SHARED, close, pthread_mutex_init, pthread_mutex_lock, pthread_mutex_t,
+    pthread_mutex_unlock, pthread_mutexattr_destroy, pthread_mutexattr_init,
+    pthread_mutexattr_setpshared, pthread_mutexattr_t,
+};
+use nix::sys::mman::{MapFlags, ProtFlags, mmap, shm_unlink};
 use nix::{
     Error,
     fcntl::OFlag,
@@ -10,32 +18,54 @@ use nix::{
     sys::{mman::shm_open, stat::Mode},
     unistd::ftruncate,
 };
+
+#[repr(C)]
 pub struct IpcArc<T> {
+    name: String,
     counter: *mut u32,
     ptr: *mut T,
+    mutex: *mut pthread_mutex_t,
+    mem_fd: Option<RawFd>,
 }
 impl<T> IpcArc<T> {
     pub fn new() -> Self {
         Self {
+            name: String::new(),
             counter: std::ptr::null_mut(),
             ptr: std::ptr::null_mut(),
+            mutex: std::ptr::null_mut(),
+            mem_fd: None,
         }
     }
 
     pub fn open(&mut self, name: &str, val: T) -> Result<(), Error> {
-        let fd = shm_open(
-            name,
-            OFlag::O_CREAT | OFlag::O_RDWR,
-            Mode::from_bits_truncate(libc::S_IRUSR | libc::S_IWUSR),
-        )
-        .expect("shm_open failed");
-        let size = 64;
+        self.name = name.to_owned();
 
-        ftruncate(&fd, size).expect("ftruncate failed");
+        // built-in to calculate allginment
+        let size = Layout::new::<(u32, libc::pthread_mutex_t, T)>().size();
+
+        let (fd, is_owner) = match shm_open(
+            name,
+            OFlag::O_CREAT | OFlag::O_RDWR | OFlag::O_EXCL,
+            Mode::from_bits_truncate(libc::S_IRUSR | libc::S_IWUSR),
+        ) {
+            Ok(fd) => {
+                ftruncate(&fd, size as i64).expect("ftruncate failed");
+                (fd, true)
+            }
+            Err(nix::Error::EEXIST) => {
+                let fd = shm_open(name, OFlag::O_RDWR, Mode::empty())?;
+                (fd, false)
+            }
+
+            Err(e) => return Err(e),
+        };
+
+        // resize the shared object
         let addr = unsafe {
             mmap(
                 None,
-                NonZero::new(20).unwrap(),
+                NonZero::new(size).unwrap(),
                 ProtFlags::PROT_READ | ProtFlags::PROT_WRITE,
                 MapFlags::MAP_SHARED,
                 &fd,
@@ -43,92 +73,164 @@ impl<T> IpcArc<T> {
             )?
         };
 
-        let nptr = addr.as_ptr() as *mut T;
+        let raw_fd = fd.into_raw_fd();
+        self.mem_fd = Some(raw_fd);
 
-        // mem layout off: 0
-        self.counter = nptr as *mut u32;
-        let counter_ptr = self.counter;
-        if self.read_counter() == 999 {
-            unsafe {
-                *counter_ptr = 0;
+        let base = addr.as_ptr() as *mut u8;
+
+        let mutex_ptr = base as *mut pthread_mutex_t;
+        let after_mutex = unsafe { base.add(size_of::<pthread_mutex_t>()) };
+        let counter_offset = after_mutex.align_offset(align_of::<u32>());
+        let counter_ptr = unsafe { after_mutex.add(counter_offset) };
+        let after_counter = unsafe { counter_ptr.add(size_of::<u32>()) };
+
+        let type_offset = after_counter.align_offset(align_of::<T>());
+        let value_ptr = unsafe { after_counter.add(type_offset) };
+
+        self.mutex = mutex_ptr;
+        self.counter = counter_ptr as *mut u32;
+        self.ptr = value_ptr as *mut T;
+
+        if is_owner {
+            unsafe { std::ptr::write_bytes(base, 0, size) };
+            println!("============= owner here ==================");
+            //----------------------- ATTR ----------------------------------//
+            // set/init mutex attr
+            let mut attr = MaybeUninit::<pthread_mutexattr_t>::uninit();
+            let ret = unsafe { pthread_mutexattr_init(attr.as_mut_ptr()) };
+            if ret != 0 {
+                panic!("pthread_mutexattr_init failed with {ret}");
             }
-        } else {
-            unsafe {
-                *counter_ptr = *counter_ptr + 1;
-            }
+
+            let mut attr = unsafe { attr.assume_init() };
+
+            // set as shared
+            let ret = unsafe {
+                pthread_mutexattr_setpshared(&attr as *const _ as *mut _, PTHREAD_PROCESS_SHARED)
+            };
+            assert_eq!(ret, 0, "pthread_mutex_init failed");
+
+            let ret = unsafe { pthread_mutex_init(mutex_ptr, &attr as *const _ as *mut _) };
+
+            unsafe { pthread_mutexattr_destroy(&mut attr) };
+            assert_eq!(ret, 0, "pthread_mutex_init failed");
         }
-        println!("count: {}", self.read_counter());
-        // offset 0 + size(u32)
-        self.ptr = unsafe { nptr.add(size_of::<u32>()) };
+        // lock
+        let ret = unsafe { pthread_mutex_lock(mutex_ptr) };
+        assert_eq!(ret, 0, "pthread_mutex_lock failed");
+
+        unsafe {
+            *self.counter = self.read_counter() + 1;
+            // println!("=====> count after: {}", *counter_ptr);
+        }
+        let ret = unsafe { pthread_mutex_unlock(mutex_ptr) };
+        assert_eq!(ret, 0, "pthread_mutex_unlock failed");
+
+        unsafe {
+            self.ptr.write(val);
+        }
 
         Ok(())
     }
+
     fn read_counter(&self) -> u32 {
-        unsafe { ptr::read(self.counter as *mut u32) }
+        unsafe { ptr::read(self.counter) }
+    }
+
+    pub fn inc_counter(&self) {
+        let mutex_ptr = self.mutex;
+        let ret = unsafe { pthread_mutex_lock(mutex_ptr) };
+        assert_eq!(ret, 0, "pthread_mutex_lock failed");
+
+        unsafe {
+            *self.counter = self.read_counter() + 1;
+            // println!("=====> count after: {}", *counter_ptr);
+        }
+        let ret = unsafe { pthread_mutex_unlock(mutex_ptr) };
+        assert_eq!(ret, 0, "pthread_mutex_unlock failed");
+    }
+
+    pub fn read_data(&self) -> T {
+        unsafe { ptr::read(self.ptr) }
+    }
+    pub fn unlink(&self) -> Result<(), Errno> {
+        unsafe { *self.counter = *self.counter - 1 };
+        if (unsafe { *self.counter } == 1) {
+            shm_unlink(self.name.as_str())?;
+        }
+        Ok(())
+    }
+    pub fn force_unlink(&self) -> Result<(), Errno> {
+        shm_unlink(self.name.as_str())?;
+        // unsafe {
+        //     munmap(addr.as_ptr(), size);
+        // }
+        unsafe { close(self.mem_fd.unwrap()) };
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
 
-    use std::{ptr, thread, time::Duration};
-
-    use nix::libc::fork;
-
-    use super::*;
     #[test]
-    fn test() {
-        let mut ins: IpcArc<u64> = IpcArc::new();
+    fn test_ipcarc_multiprocess_counter() {
+        use super::*;
 
-        ins.open("/hello", 43).unwrap();
+        use nix::unistd::{ForkResult, fork};
+        use std::process::exit;
 
-        unsafe {
-            ptr::write(ins.ptr, 43);
+        const NUM_PROCESSES: usize = 20;
+        const INCREMENTS_PER_PROCESS: usize = 900;
+
+        // Setup in parent
+        let mut shared = IpcArc::<u64>::new();
+        shared.open("/hello", 42).unwrap();
+
+        let mut children = vec![];
+
+        for _ in 0..NUM_PROCESSES {
+            match unsafe { fork() } {
+                Ok(ForkResult::Child) => {
+                    let mut child = IpcArc::<u64>::new();
+                    child.open("/hello", 99).unwrap();
+
+                    for _ in 0..INCREMENTS_PER_PROCESS {
+                        child.inc_counter();
+                    }
+
+                    // Optional debug
+                    println!(
+                        "[child {}] counter: {}",
+                        std::process::id(),
+                        child.read_counter()
+                    );
+
+                    // Do NOT unlink in child
+                    exit(0);
+                }
+                Ok(ForkResult::Parent { child, .. }) => {
+                    children.push(child);
+                }
+                Err(e) => panic!("fork failed: {e}"),
+            }
         }
 
-        let reference: &mut u64 = unsafe { &mut *ins.ptr };
-        println!("re:{}", *reference);
-
-        let s = unsafe { fork() };
-        if s == 0 {
-            thread::sleep(Duration::from_secs(1));
-
-            unsafe {
-                ptr::write(ins.ptr, 1111);
-            }
-            println!("hehe: {s}");
-            ins.open("/hello", 43).unwrap();
-
-            let reference: &mut u64 = unsafe { &mut *ins.ptr };
-            println!("re:{}", *reference);
-        } else {
-            thread::sleep(Duration::from_secs(2));
-            println!("else: {s}");
-            ins.open("/hello", 43).unwrap();
-
-            let reference: &mut u64 = unsafe { &mut *ins.ptr };
-
-            unsafe {
-                ptr::write(ins.ptr, 88);
-            }
-            println!("re:{}", *reference);
+        // Wait for children
+        for pid in children {
+            nix::sys::wait::waitpid(pid, None).expect("waitpid failed");
         }
 
-        println!("re:{}", *reference);
-        // unsafe {
-        //     assert!(!ins.ptr.is_null(), "Pointer is null");
-        //     assert_eq!(
-        //         (ins.ptr as usize) % std::mem::align_of::<u64>(),
-        //         0,
-        //         "Pointer is misaligned"
-        //     );
-        //
-        //     // Write to mmaped memory safely
-        //     std::ptr::write(ins.ptr, 43u64);
-        //
-        //     // Read it back
-        //     let reference: &mut u64 = &mut *ins.ptr;
-        //     println!("re:{}", *reference);
-        // }
+        let final_counter = shared.read_counter();
+        let expected = (NUM_PROCESSES * INCREMENTS_PER_PROCESS + 20 + 1) as u32;
+
+        println!("[parent] final counter: {}", final_counter);
+
+        shared.force_unlink().unwrap();
+        assert_eq!(
+            final_counter, expected,
+            "Expected {}, got {}",
+            expected, final_counter
+        );
     }
 }
